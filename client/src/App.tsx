d090b1da76bfb10818abc03e08c9fa45938f900e
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2026-02-18 20:18:50 -0300

    fix config attemptive 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,22 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Router as WRouter, Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import Home from "./pages/Home";
 
-
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
+      <WRouter base="/neeples">
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/404" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </WRouter>
     </Switch>
   );
 }
