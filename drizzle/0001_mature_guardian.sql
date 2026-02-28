48867c299244fef68b467cbb1b60e857bda3d5a6
Neeples <neeeeples@gmail.com>
2026-02-28 07:49:46 -0300

    Delete drizzle directory

--- drizzle/0001_mature_guardian.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `content` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`language` varchar(10) NOT NULL DEFAULT 'en',
-	`key` varchar(255) NOT NULL,
-	`value` text NOT NULL,
-	`createdAt` timestamp NOT NULL DEFAULT (now()),
-	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT `content_id` PRIMARY KEY(`id`)
-);
